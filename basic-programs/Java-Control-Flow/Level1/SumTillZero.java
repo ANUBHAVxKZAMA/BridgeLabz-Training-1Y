9eb557a249a13284fdb56342c27f2003dccfbd83
Anubhav Srivastav <anubhavsrivastav12398@gmail.com>
2026-01-30 11:41:55 +0530

    Added solutions to the given Level1 problems

---
+++ basic-programs/Java-Control-Flow/Level1/SumTillZero.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class SumTillZero{
+	public static void main (String args[]){
+		Scanner sc = new Scanner(System.in);
+		int x,s=0;
+		System.out.println("Enter number(s):");
+		while(true)
+		{
+		    x=sc.nextInt();
+			if(x!=0)
+				s+=x;
+			else
+				break;
+		}
+		System.out.println("Sum= "+s);
+	}
+}
